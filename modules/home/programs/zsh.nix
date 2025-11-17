956e07318bfc14231794cb4714388812051756fd
Anatou <anatole@desnot.com>
2025-11-17 17:48:26 +0100

    added zsh shortcuts

--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -19,7 +19,9 @@
                 gs = "git status";
                 gadd = "git add .";
                 gps = "git push";
+                push = "git push";
                 gpl = "git pull";
+                pull = "git pull";
                 gco = "git commit -m ";
                 glg = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)%ad (%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all --date=format:%Y-%m-%d@%H:%M:%S";
                 hm = "home-manager";
